e1b9a44318eb152ef68415c95fb770af2a7d76e7
gitrunner-aju <adityajujalambkar@gmail.com>
2026-07-02 12:01:17 +0530

    Resolved

--- 01-Arrays/005-Majority-element/Solution.java
+++ 01-Arrays/005-Majority-element/Solution.java
@@ -1,28 +1,29 @@
 public class Solution {
   
-  public static int findMajority(int[] arr){
+  public static int findMajority(int[] nums){
 
     int ME=0,count=0;
-    ME = arr[0];
+    ME = nums[0];
     count = 1;
 
-    for(int i=1;i<arr.length;i++){
-      if(ME==arr[i]){
+    for(int i=1;i<nums.length;i++){
+      if(ME==nums[i]){
         count++;
       }else{
         count--;
         if(count==0){
-          ME = arr[i];
+          ME = nums[i];
+          count=1;
         }
       }
     }
     int count2=0;
-    for(int i=0;i<arr.length;i++){
-      if(ME==arr[i]){
+    for(int i=0;i<nums.length;i++){
+      if(ME==nums[i]){
         count2++;
       }
     }
-    if(count2>arr.length/2){
+    if(count2>nums.length/2){
       return ME;
     }else{
       return -1;
@@ -31,7 +32,7 @@ public class Solution {
   }
 
   public static void main(String[] args) {
-    int[] arr = {2,2,1,1,1,2,2};
-    System.out.println(findMajority(arr));
+    int[] nums = {2,2,1,1,1,2,2};
+    System.out.println(findMajority(nums));
   }
 }
