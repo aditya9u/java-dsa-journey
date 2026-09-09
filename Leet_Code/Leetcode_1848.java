e25084eda5fb0662380796c40b9f0e13d5dd148c
aditya9u <aditya9ujalambkar@gmail.com>
2026-09-09 13:58:25 +0530

    leetcode problem solving

---
+++ Leet_Code/Leetcode_1848.java
@@ -0,0 +1,17 @@
+package Leet_Code;
+
+public class Leetcode_1848 {
+
+     public int getMinDistance(int[] nums, int target, int start) {
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+               min = Math.min(Math.abs(i-start),min);
+            }
+        }
+
+        return min;
+
+    }
+    
+}
